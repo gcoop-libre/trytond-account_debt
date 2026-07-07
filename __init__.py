134ac7fa08c55daf97b5f7179a59325a871ed922
Luciano Rossi <lukio@gcoop.coop>
2026-07-07 20:12:25 -0300

    Prepare release 8.0

--- __init__.py
+++ __init__.py
@@ -1,18 +1,3 @@
 # This file is part of the account_debt module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from trytond.pool import Pool
-from . import account
-
-
-def register():
-    Pool.register(
-        account.AccountDebtStart,
-        module='account_debt', type_='model')
-    Pool.register(
-        account.AccountDebt,
-        module='account_debt', type_='wizard')
-    Pool.register(
-        account.AccountDebtReport,
-        module='account_debt', type_='report')
